371ce7c4b79122067d850d6c977d83ba3301dc89
wendellhernandez <wendellhbusiness@gmail.com>
2024-03-08 10:20:23 +0800

    finished Multiplayer Game. Starting Frameworks

--- Javascript-Track/Socket.io/Multiplayer Game/public/js/script.js
+++ Javascript-Track/Socket.io/Multiplayer Game/public/js/script.js
@@ -1,65 +1,110 @@
-const socket = io();
-/*
-    Docu: Game Variables
-    Owner: Wendell
-*/
-let background_position = 100;
-let enemies = [];
-let players = [];
-let projectile_counter = 0
-let player_update = {};
-/*
-    Docu: Get game variables from the server every 14ms
-    Owner: Wendell
-*/
-socket.on('update_client' , function(data){
-    background_position = data.background_position;
-    players = data.players;
-    enemies = data.enemies;
-})
-/*
-    Docu: Send game variables to the server every 14ms
-    Owner: Wendell
-*/
-function update(){
-    socket.emit('update_server' , player_update);
+class Game{
+    constructor(socket){
+        /*
+            Docu: Game Variables
+            Owner: Wendell
+        */
+        this.socket = socket;
+        this.background_position = 100;
+        this.enemies = [];
+        this.players = [];
+        this.projectile_counter = 0
+        this.player_update = {};
+    }
     /*
-        Docu: updates the background position so it moves up
+        Docu: Send game variables to the server every 13ms
         Owner: Wendell
     */
-    $('main').css('background-position' , `0% ${background_position}%`);
-    $('main').html('');
-
-    for(const player of players){
-        let elem = $('<div>');
-        elem.addClass('player');
-        elem.css({'top' : player.yAxis , 'left' : player.xAxis});
-        $('main').append(elem);
-
-        for(const projectile of player.projectiles){
-            let elem = $('<div>');
-            elem.addClass('projectile');
-            elem.css({'top' : projectile.yAxis , 'left' : projectile.xAxis});
-            $('main').append(elem);
-        }
+    update(){
+        setInterval(function(){
+            game.socket.emit('update_server' , game.player_update);
+            /*
+                Docu: updates the background position so it moves up
+                Owner: Wendell
+            */
+            $('main').css('background-position' , `0% ${game.background_position}%`);
+            $('main').html('');
+            $('.score_board').html('<p class="title">SCORE BOARD</p>');
+            /*
+                Docu: Adds players planes to the game
+                Owner: Wendell
+            */
+            for(const player of game.players){
+                let elem = $('<div>');
+                elem.addClass('player');
+                elem.css({'top' : player.yAxis , 'left' : player.xAxis});
+                elem.html(`<span>${player.id.substring(0,6)}</span>`);
+                $('main').append(elem);
+                /*
+                    Docu: Adds player scores to the game
+                    Owner: Wendell
+                */
+                let elem2 = $('<div>');
+                elem2.addClass('score');
+                elem2.html(`${player.id.substring(0,6)}: ${player.score}`)
+                $('.score_board').append(elem2);
+                /*
+                    Docu: Adds player bullets to the game
+                    Owner: Wendell
+                */
+                for(const projectile of player.projectiles){
+                    let elem = $('<div>');
+                    elem.addClass('projectile');
+                    elem.css({'top' : projectile.yAxis , 'left' : projectile.xAxis});
+                    $('main').append(elem);
+                }
+            }
+            /*
+                Docu: Adds enemy planes to the game
+                Owner: Wendell
+            */
+            for(const enemy of game.enemies){
+                if(enemy){
+                    let elem = $('<div>');
+                    elem.addClass('enemy');
+                    elem.css({'top' : enemy.yAxis , 'left' : enemy.xAxis});
+                    elem.html(`<span>${enemy.health}</span>`);
+                    $('main').append(elem);
+                }
+            }
+        } , 13);
     }
-
-    for(const enemy of enemies){
-        let elem = $('<div>');
-        elem.addClass('enemy');
-        elem.css({'top' : enemy.yAxis , 'left' : enemy.xAxis});
-        $('main').append(elem);
+    /*
+        Docu: Get game variables from the server every 13ms
+        Owner: Wendell
+    */
+    update_client(){
+        game.socket.on('update_client' , function(data){
+            game.background_position = data.background_position;
+            game.players = data.players;
+            game.enemies = data.enemies;
+        })
+        /*
+            Docu: Change player position using mouse pointer event
+            Owner: Wendell
+        */
+        $('main').mousemove(function(e){
+            game.player_update.xAxis = e.offsetX-34;
+            game.player_update.yAxis = e.offsetY-24;
+        })
+        /*
+            Docu: Shoots when f key is pressed
+            Owner: Wendell
+        */
+        $(document).keydown(function(e){
+            if(e.key == 'f'){
+                socket.emit('shoot');
+            }
+        })
+        
+        $('button').click(function(){
+            socket.emit('join');
+        })
     }
-
-    requestAnimationFrame(update);
 }
 
-// requestAnimationFrame(update);
-/*
-    Docu: Change player position using mouse pointer event
-    Owner: Wendell
-*/
-$('main').mousemove(function(e){
-    player_update.xAxis = e.offsetX-34;
-    player_update.yAxis = e.offsetY-24;
-})
+const socket = io();
+const game = new Game(socket);
+
+game.update();
+game.update_client();
--- Javascript-Track/Socket.io/Multiplayer Game/server.js
+++ Javascript-Track/Socket.io/Multiplayer Game/server.js
@@ -9,84 +9,196 @@ app.use(express.static(__dirname + "/public"));
 app.set('views', __dirname + '/public/views');
 app.set('view engine', 'ejs');
 
+const scale = 2.5;
 let background_position = 100;
-let enemy_initial_health = 10;
 let enemies = [];
 let players = [];
-let projectile_counter = 0
+let projectile_timer = 0
 
-io.on('connection' , function(socket){
-    players.push(
-        {
-        id: socket.id,
-        xAxis: 0,
-	    yAxis: 0,
-        projectiles: [
-            {
-                xAxis: 0,
-                yAxis: 0,
-            }
-        ]
-        }
-    )
+/*
+    Docu: Adds enemies in random positions 
+            every set amount of time
+    Owner: Wendell
+*/
+function getRandHealth(){
+    return Math.floor(Math.random() * 10) + 10;
+}
 
+function getRandX(){
+    return Math.floor(Math.random() * 260 * scale);
+}
+
+setInterval(function(){
     enemies.push(
         {
-            xAxis: 0,
-	        yAxis: 0,
-            health: enemy_initial_health
+            xAxis: getRandX(),
+            yAxis: 0,
+            health: getRandHealth(),
+            direction: Math.round(Math.random()) * 2 - 1
         }
     )
+} , 500);
 
-    setInterval(function(){
-        enemies.push(
-            {
-                xAxis: 0,
-                yAxis: 0,
-                health: 10
-            }
-        )
-    } , 3000);
-
-    socket.on('update_server' , function(player_update){
-        projectile_counter++;
-
-        for(const player of players){
-            if(player.id === socket.id){
-                player.xAxis = player_update.xAxis;
-                player.yAxis = player_update.yAxis;
-            }
-
-            if(projectile_counter > 10){
-                projectile_counter = 0;
-                if(player_update.xAxis){
-                    player.projectiles.push(
-                        {
-                            xAxis: player_update.xAxis + 31,
-                            yAxis: player_update.yAxis - 5,
-                        }
-                    )
+io.on('connection' , function(socket){
+    /*
+        Docu: Adds player when join button is clicked
+        Owner: Wendell
+    */
+    socket.on('join' , function(){
+        function find_player(){
+            for(const player of players){
+                if(player.id === socket.id){
+                    return true;
                 }
             }
 
+            return false;
+        }
+
+        if(!find_player()){
+            players.push(
+                {
+                id: socket.id,
+                xAxis: 0,
+                yAxis: 0,
+                projectiles: [
+                    {
+                        xAxis: 0,
+                        yAxis: 0,
+                    }
+                ],
+                score: 0
+                }
+            )
+        }
+    })
+    /*
+        Docu: Creates projectiles when player is shooting
+        Owner: Wendell
+    */
+    socket.on('shoot' , function(){
+        for(const player of players){
+            if(player.id === socket.id){
+                player.projectiles.push(
+                    {
+                        xAxis: player.xAxis + 16 * scale,
+                        yAxis: player.yAxis - 3 * scale,
+                    }
+                )
+            }
+        }
+    })
+
+    socket.on('update_server' , function(player_update){
+        projectile_timer++;
+
+        for(const player of players){
+            /*
+                Docu: Updates player movements
+                Owner: Wendell
+            */
+            if(player.id === socket.id){
+                if(player_update.xAxis){
+                    player.xAxis = player_update.xAxis;
+                    player.yAxis = player_update.yAxis;
+                }
+            }
+            /*
+                Docu: Slides all projectiles upwards and removes them
+                        if they go off-screen
+                Owner: Wendell
+            */
             for(let i=0; i<player.projectiles.length; i++){
-                player.projectiles[i].yAxis -= 6;
+                player.projectiles[i].yAxis -= 1.5 * scale;
 
                 if(player.projectiles[i].yAxis < 0){
                     player.projectiles.splice(i,1);
                 }
             }
         }
+        /*
+            Docu: Set enemy movements and removes them if they 
+                    go off-screen
+            Owner: Wendell
+        */
+        for(let i=0; i<enemies.length; i++){
+            if(enemies[i].xAxis){
+                for(let j=0; j<players.length; j++){
+                    for(let k=0; k<players[j].projectiles.length; k++){
+                        if(
+                            (players[j].projectiles[k].xAxis > enemies[i].xAxis
+                            && 
+                            players[j].projectiles[k].xAxis < enemies[i].xAxis + 12 * scale)
 
+                            &&
+
+                            (players[j].projectiles[k].yAxis > enemies[i].yAxis
+                                && 
+                                players[j].projectiles[k].yAxis < enemies[i].yAxis + 12 * scale)
+                        ){
+                            enemies[i].health--;
+                            players[j].projectiles.splice(k,1);
+                            players[j].score += 1;
+                        }
+                    }
+
+                    if(
+                        (enemies[i].xAxis + 8 * scale> players[j].xAxis
+                        && 
+                        enemies[i].xAxis + 8 * scale < players[j].xAxis + 34 * scale)
+
+                        &&
+
+                        (enemies[i].yAxis + 12 * scale > players[j].yAxis
+                        && 
+                        enemies[i].yAxis + 12 * scale < players[j].yAxis + 24 * scale)
+                    ){
+                        players.splice(j,1);
+                    }
+                }
+                /*
+                    Docu: Moves the enemies down, left or right and
+                            removes enemies when off-screen
+                    Owner: Wendell
+                */
+                enemies[i].xAxis += enemies[i].direction * 0.3;
+                enemies[i].yAxis += 3;
+
+                if(enemies[i].yAxis > 363 * scale){
+                    enemies.splice(i,1);
+                }
+                /*
+                    Docu: Removes enemies when health is zero
+                    Owner: Wendell
+                */
+                if(enemies[i].health != undefined){
+                    if(enemies[i].health < 1){
+                        enemies.splice(i,1);
+                    }
+                }
+            }
+        }
+        /*
+            Docu: Slides the background image upward for all players
+                    and resets
+            Owner: Wendell
+        */
         if(background_position > 0){
-            background_position -= 0.01;
+            background_position -= 0.005;
         }else{
             background_position = 100;
         }
-
+        /*
+            Docu: Sends players, background positions and enemies to the client
+                    for update every 14ms
+            Owner: Wendell
+        */
         io.emit('update_client' , {players , background_position , enemies});
     });
-
+    /*
+        Docu: Listen for disconnect and removes player
+        Owner: Wendell
+    */
     socket.on('disconnect' , function(){
         for(let i=0; i<players.length; i++){
             if(players[i].id === socket.id){
@@ -95,7 +207,10 @@ io.on('connection' , function(socket){
         }
     })
 })
-
+/*
+    Docu: Express routing. Render index page
+    Owner: Wendell
+*/
 app.get('/', function(req, res) {
     res.render("index");
 })
